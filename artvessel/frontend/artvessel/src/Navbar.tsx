852b4e6623449d42bbc864dd3625d7ba28e8c8fe
Ellie <yokomone@yahoo.com>
2023-11-15 03:45:37 +0200

    added navbar

---
+++ artvessel/frontend/artvessel/src/Navbar.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import logo from "./images/logo.png"
+import logo_text from "./images/logo-text.png"
+import default_pfp from "./images/default-pfp.png"
+import './css/navbar.css'
+
+function Navbar() {
+    return  (
+    <div className="navbar">
+        <div className="logo">
+            <a href="#">
+                <img className="logo-image" src={logo} alt="logo"></img>
+                <img className="logo-text" src={logo_text} alt="logo-text"></img>
+            </a>
+        </div>
+        <div className="right">
+            <div className="guest">
+                <a href="#">LOG IN</a>
+                <a href="#">SIGN UP</a>
+            </div>
+            <div className="logged-in">
+                <a href="#" title="Saved"><svg className="saved" xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"/></svg></a>
+                <a href="#"><div className="pfp"><img src={default_pfp} alt="profile picture"></img></div></a>
+            </div>
+        </div>
+    </div>)
+}
+
+export default Navbar
