995f9dd4a717962a41497f86b22691d18ea2fabd
Ellie <yokomone@yahoo.com>
2023-11-15 18:22:06 +0200

    added profile cards to homepage

--- artvessel/frontend/artvessel/src/App.jsx
+++ artvessel/frontend/artvessel/src/App.jsx
@@ -1,7 +1,13 @@
+import ProfileCards from "./ProfileCards";
 import Navbar from "./Navbar";
 
 function App() {
-  return <><Navbar/></>
+  return (
+  <>
+    <Navbar/>
+    <ProfileCards/>
+  </>
+  )
 }
 
 export default App
