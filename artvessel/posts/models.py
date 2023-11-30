ebbf723960ca0f4ed5aca23a2436f39371f8094b
Ellie <yokomone@yahoo.com>
2023-12-01 00:18:37 +0200

    added shop post

--- artvessel/posts/models.py
+++ artvessel/posts/models.py
@@ -6,7 +6,17 @@ from posts.validators import validate_file_size
 class GalleryPost(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    title = models.CharField(max_length=50)
     drawing = models.ImageField(upload_to='images', validators=(validate_file_size,))
+    description = models.TextField(max_length=2000, blank=True, null=True)
+
+
+class ShopPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='images', validators=(validate_file_size,))
     description = models.TextField(max_length=2000, blank=True, null=True)
 
 
