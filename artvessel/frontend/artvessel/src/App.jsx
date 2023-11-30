424600af10d3e1e599aefd60ded7cfc0cf753de8
Ellie <yokomone@yahoo.com>
2023-11-30 04:32:21 +0200

    added footer

--- artvessel/frontend/artvessel/src/App.jsx
+++ artvessel/frontend/artvessel/src/App.jsx
@@ -1,11 +1,13 @@
 import ProfileCards from "./ProfileCards";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function App() {
   return (
   <>
     <Navbar/>
     <ProfileCards/>
+    <Footer/>
   </>
   )
 }
---
+++ artvessel/frontend/artvessel/src/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import './css/footer.css'
+
+
+function Footer(){
+    return <>
+        <div className="footer">
+            <a href="">Mone</a>
+        </div>
+    </>
+}
+
+export default Footer
