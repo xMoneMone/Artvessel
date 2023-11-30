6720c48c03bc61e6cc977a2ca7a1b251f20d4a8c
Ellie <yokomone@yahoo.com>
2023-11-30 23:51:09 +0200

    added userprofile model

---
+++ artvessel/userprofile/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from userprofile.models import UserProfile
+
+
+def shorten(string, val: int):
+    if not string:
+        return None
+    if len(string) > val:
+        return string[:val] + "..."
+    else:
+        return string
+
+
+@admin.register(UserProfile)
+class USerProfileAdmin(admin.ModelAdmin):
+    list_display = ("id", "username", "bio_pv")
+
+    def username(self, obj):
+        username = str(obj.user)
+        return shorten(username, 10)
+
+    def nick(self, obj):
+        return shorten(obj.nickname, 12)
+
+    def bio_pv(self, obj):
+        return shorten(obj.bio, 20)
---
+++ artvessel/userprofile/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+from userprofile.validators import validate_file_size
+from django.core.validators import MaxValueValidator
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    profile_pic = models.ImageField(upload_to='images', validators=(validate_file_size,), blank=True, null=True)
+    cover_pic = models.ImageField(upload_to='images', validators=(validate_file_size,), blank=True, null=True)
+    bio = models.TextField(max_length=150, blank=True, null=True)
+    location = models.CharField(max_length=45, blank=True, null=True)
+    phone = models.IntegerField(validators=[MaxValueValidator(999999999999999999)], blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    link1 = models.URLField(blank=True, null=True)
+    link2 = models.URLField(blank=True, null=True)
+    link3 = models.URLField(blank=True, null=True)
+    link4 = models.URLField(blank=True, null=True)
+    link5 = models.URLField(blank=True, null=True)
+    shop_info = models.TextField(max_length=2000, blank=True, null=True)
+
