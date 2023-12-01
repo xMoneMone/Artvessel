d03c96fca288dcd2ec7469de9dfd89e94342286d
Ellie <yokomone@yahoo.com>
2023-12-01 02:28:46 +0200

    added django cleanup

--- artvessel/artvessel/settings.py
+++ artvessel/artvessel/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userprofile',
-    'posts'
+    'posts',
+    'django_cleanup.apps.CleanupConfig'
 ]
 
 MIDDLEWARE = [
