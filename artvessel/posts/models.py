9cb6a0a428dfb40bd67bdfd9fa7bd6a58727e270
Ellie <yokomone@yahoo.com>
2023-12-02 22:38:38 +0200

    added priority to gallerypost

--- artvessel/posts/models.py
+++ artvessel/posts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from posts.validators import validate_file_size
+from django.core.validators import MaxValueValidator
 
 
 class GalleryPost(models.Model):
@@ -9,6 +10,7 @@ class GalleryPost(models.Model):
     title = models.CharField(max_length=50)
     drawing = models.ImageField(upload_to='images', validators=(validate_file_size,))
     description = models.TextField(max_length=2000, blank=True, null=True)
+    priority = models.IntegerField(validators=([MaxValueValidator(1000000)]), default=0)
 
 
 class ShopPost(models.Model):
