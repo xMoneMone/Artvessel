3bd5e63529cc7d6578e3ad2430750b1d78ee80b4
Ellie <yokomone@yahoo.com>
2023-12-04 21:18:06 +0200

    created post details api

--- artvessel/posts/urls.py
+++ artvessel/posts/urls.py
@@ -4,7 +4,7 @@ from posts import views
 urlpatterns = [
     path('posts', views.all_posts, name="all-posts"),
     path('shop', views.all_shop, name="all-shop"),
-    path('posts/<int:pk>', views.post, name="post"),
-    path('posts/<int:pk>/save', views.post_save, name="post-save"),
+    path('post/<int:pk>', views.post, name="post"),
+    path('post/<int:pk>/save', views.post_save, name="post-save"),
     path('shop/<int:pk>', views.shop, name="shop")
 ]
--- artvessel/posts/views.py
+++ artvessel/posts/views.py
@@ -1,4 +1,8 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+
+from posts.models import GalleryPost
+from posts.serialisers import post_serializer
 
 
 def all_posts(request):
@@ -9,8 +13,10 @@ def all_shop(request):
     return
 
 
-def post(request):
-    return
+def post(request, pk):
+    if GalleryPost.objects.filter(pk=pk).exists():
+        post = GalleryPost.objects.get(pk=pk)
+    return JsonResponse(post_serializer(request.get_host(), post))
 
 
 def shop(request):
