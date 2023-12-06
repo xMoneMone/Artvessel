8a7e85152e4f7bfcabb8ae42acd961b6c0560480
Ellie <yokomone@yahoo.com>
2023-12-06 02:39:57 +0200

    added create type selection

--- artvessel/frontend/artvessel/src/App.jsx
+++ artvessel/frontend/artvessel/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./Footer";
 import Profile from "./Profile";
 import React, { useEffect, useState } from "react";
 import Create from "./Create";
+import CreatePost from "./CreatePost";
 import CreateShop from "./CreateShop";
 import PostDetails from "./PostDetails";
 
@@ -16,16 +17,17 @@ function App() {
   const [user, setUser] = useState("")
 
   useEffect(() => {
-        fetch("http://127.0.0.1:8000/users-api/user")
-        .then(res => {
-            return res.json()
-        })
-        .then((data) => {
-            setUser(data)
-        })
-        .catch((err) => {
-            console.log(err.message)
-        })
+        // fetch("http://127.0.0.1:8000/users-api/user")
+        // .then(res => {
+        //     return res.json()
+        // })
+        // .then((data) => {
+        //     setUser(data)
+        // })
+        // .catch((err) => {
+        //     console.log(err.message)
+        // })
+        setUser("admin")
   }, [])
 
   return (
@@ -38,9 +40,10 @@ function App() {
             <Route exact path="/" element={<Home/>} title="Home | Artvessel" />
             <Route exact path="/login" element={<Login/>} title="Log in | Artvessel" />
             <Route exact path="/signup" element={<Signup/>} title="Sign up | Artvessel" />
-            <Route exact path="/post/create" element={<Create/>} title="Create | Artvessel" />
+            <Route exact path="/create/post" element={<CreatePost/>} title="Create | Artvessel" />
+            <Route exact path="/create" element={<Create/>} title="Create | Artvessel" />
             <Route exact path="/post/:pk" element={<PostDetails/>} title="Create | Artvessel" />
-            <Route exact path="/shop/create" element={<CreateShop/>} title="Create | Artvessel" />
+            <Route exact path="/create/shop" element={<CreateShop/>} title="Create | Artvessel" />
             <Route exact path="/:username" element={<Profile section="gallery"/>} title="Profile | Artvessel" />
             <Route exact path="/:username/shop" element={<Profile section="shop"/>} title="Profile | Artvessel" />
             <Route exact path="/:username/info" element={<Profile section="info"/>} title="Profile | Artvessel" />
