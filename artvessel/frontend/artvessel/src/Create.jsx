fc1c38d79dfd3b061eccee5df4f597e76dfcdfaa
xMoneMone <92688848+xMoneMone@users.noreply.github.com>
2023-12-08 22:04:26 +0200

    cant access create without being logged in

--- artvessel/frontend/artvessel/src/Create.jsx
+++ artvessel/frontend/artvessel/src/Create.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./css/form.css"
 import Button from "./Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Create() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!document.cookie.replace("current_user=", "")){
+           navigate("/login")
+        }   
+      }, [])
+
     return <>
         <div className="form-container">
             <Link to="post" className="create-button"><Button>NEW POST</Button></Link>
